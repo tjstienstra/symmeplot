56c80a769466b1ba581f268ca9a89220baf2b6f0
tjstienstra <timostienstra00@gmail.com>
2024-01-30 21:28:44 +0100

    Update documentation

--- src/symmeplot/pyqtgraph/plot_objects.py
+++ src/symmeplot/pyqtgraph/plot_objects.py
@@ -35,26 +35,6 @@ class PlotPoint(PlotPointMixin, PgPlotBase):
         Kwargs that are parsed to :class:`pyqtgraph.opengl.items.GLScatterPlotItem`, so
         ``color=(1, 0, 0, 1)`` will make the plotted point red.
 
-    Examples
-    --------
-    .. jupyter-execute::
-
-        import matplotlib.pyplot as plt
-        import sympy as sm
-        import sympy.physics.mechanics as me
-        from symmeplot.matplotlib import PlotPoint
-
-        l1, l2, l3 = sm.symbols("l:3")
-        N, O = me.ReferenceFrame("N"), me.Point("O")
-        P1 = O.locatenew("P1", (l1 * N.x + l2 * N.y + l3 * N.z))
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        plot_point = PlotPoint(N, O, P1, color="k")
-        f = sm.lambdify((l1, l2, l3), plot_point.get_expressions_to_evaluate())
-        plot_point.values = f(0, 0, 0)
-        plot_point.plot()  # Plot the point
-        plot_point.values = f(0.2, 0.6, 0.3)
-        plot_point.update()  # The point will now be on its new position
-
     """
 
     def __init__(self, inertial_frame: ReferenceFrame, zero_point: Point,
@@ -84,27 +64,6 @@ class PlotLine(PlotLineMixin, PgPlotBase):
     **kwargs : dict, optional
         Kwargs that are parsed to :class:`pyqtgraph.opengl.items.GLLinePlotItem`, so
         ``color=(1, 0, 0, 1)`` will make the plotted line red.
-
-    Examples
-    --------
-    .. jupyter-execute::
-
-        import matplotlib.pyplot as plt
-        import sympy as sm
-        import sympy.physics.mechanics as me
-        from symmeplot.matplotlib import PlotLine
-
-        l1, l2, l3 = sm.symbols("l:3")
-        N, O = me.ReferenceFrame("N"), me.Point("O")
-        P1 = O.locatenew("P1", (l1 * N.x + l2 * N.y + l3 * N.z))
-        P2 = P1.locatenew("P2", -0.3 * N.x)
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        line_plot = PlotLine(N, O, [O, P1, P2], color="k")
-        f = sm.lambdify((l1, l2, l3), line_plot.get_expressions_to_evaluate())
-        line_plot.values = f(0, 0, 0)
-        line_plot.plot()  # Plot the point
-        line_plot.values = f(0.2, 0.6, 0.3)
-        line_plot.update()  # The point will now be on its new position
 
     """
 
@@ -137,24 +96,6 @@ class PlotVector(PlotVectorMixin, PgPlotBase):
     **kwargs : dict, optional
         Kwargs that are parsed to :class:`pyqtgraph.opengl.items.GLLinePlotItem`, so
         ``color=(1, 0, 0, 1)`` will make the plotted vector red.
-
-    Examples
-    --------
-    .. jupyter-execute::
-
-        import matplotlib.pyplot as plt
-        import sympy as sm
-        import sympy.physics.mechanics as me
-        from symmeplot.matplotlib import PlotVector
-
-        N = me.ReferenceFrame("N")
-        O = me.Point("O")
-        O_v = O.locatenew("O_v", 0.2 * N.x + 0.2 * N.y + 0.7 * N.z)
-        v = 0.4 * N.x + 0.4 * N.y - 0.6 * N.z
-        v_plot = PlotVector(N, O, v, O_v, color="r", ls="--")
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        v_plot.values = sm.lambdify((), v_plot.get_expressions_to_evaluate())()
-        v_plot.plot(ax)
 
     """
 
@@ -195,28 +136,6 @@ class PlotFrame(PlotFrameMixin, PgPlotBase):
         :class:`symmeplot.pyqtgraph.plot_objects.PlotVector`s, which possibly parses
         them to :class:`pyqtgraph.opengl.items.GLLinePlotItem`, so
         ``color=(1, 0, 0, 1)`` will make all vectors of the reference frame red.
-
-    Examples
-    --------
-    .. jupyter-execute::
-
-        import matplotlib.pyplot as plt
-        import sympy as sm
-        import sympy.physics.mechanics as me
-        from symmeplot.matplotlib import PlotFrame
-
-        N = me.ReferenceFrame("N")
-        A = me.ReferenceFrame("A")
-        A.orient_axis(N, N.z, 1)
-        N0 = me.Point("N_0")
-        A0 = N0.locatenew("A_0", 0.2 * N.x + 0.2 * N.y + 0.7 * N.z)
-        N_plot = PlotFrame(N, N0, N, scale=0.5)
-        A_plot = PlotFrame(N, N0, A, A0, scale=0.5, ls="--")
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        N_plot.values = sm.lambdify((), N_plot.get_expressions_to_evaluate())()
-        A_plot.values = sm.lambdify((), A_plot.get_expressions_to_evaluate())()
-        N_plot.plot(ax)
-        A_plot.plot(ax)
 
     """
 
@@ -277,33 +196,6 @@ class PlotBody(PlotBodyMixin, PgPlotBase):
         mass.
     **kwargs : dict, optional
         Kwargs that are parsed to both internally used plot objects.
-
-    Examples
-    --------
-    .. jupyter-execute::
-
-        import matplotlib.pyplot as plt
-        import sympy as sm
-        import sympy.physics.mechanics as me
-        from symmeplot.matplotlib import PlotBody
-
-        N = me.ReferenceFrame("N")
-        A = me.ReferenceFrame("A")
-        A.orient_axis(N, N.z, 1)
-        N0 = me.Point("N_0")
-        A0 = N0.locatenew("A_0", 0.2 * N.x + 0.2 * N.y + 0.7 * N.z)
-        ground = me.RigidBody("ground", N0, N, 1, (N.x.outer(N.x), N0))
-        body = me.RigidBody("body", A0, A, 1, (A.x.outer(A.x), A0))
-        ground_plot = PlotBody(N, N0, ground)
-        body_plot = PlotBody(N, N0, body)
-        body_plot.attach_circle(body.masscenter, 0.3, A.x + A.y + A.z,
-                                facecolor="none", edgecolor="k")
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        ground_plot.values = sm.lambdify((), ground_plot.get_expressions_to_evaluate()
-                                        )()
-        body_plot.values = sm.lambdify((), body_plot.get_expressions_to_evaluate())()
-        ground_plot.plot(ax)
-        body_plot.plot(ax)
 
     """
 
